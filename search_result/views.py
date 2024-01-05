0223eb748724f2bf358a5e17c73015644b88d759
Lumen611727 <ylee7385@gmail.com>
2024-01-06 00:22:08 +0800

    little adjust

--- search_result/views.py
+++ search_result/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def search_result_view(request):
     if request.method == "POST":
-        mydata = ["this", "is", "where", "you", "put", "result", "in"]
+        mydata = ["this", "is", "where", "you", "put", "result", "in"] #mydata stores the result of query, use 'request.POST.get("searchKey")' to get keyword
         return render(request, 'search_result.html', {
         'data': "Below are searching result for: "+ request.POST.get('searchKey'), 
         'context': mydata,
