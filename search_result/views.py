2659646760ef7acf0e738e5af3418a01ebb570b6
yl622 <82879562+yl622@users.noreply.github.com>
2024-01-11 12:42:17 +0800

    Update views.py

--- search_result/views.py
+++ search_result/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Books_rating,books_data
-from django.db.models import F
+from .models import Books_rating,books_data,Books,Ratings,Users
+from django.db.models import F,Q
 # Create your views here.
 from django.db.models import Avg,Min,Max,Count
 
@@ -24,11 +24,25 @@ def search_result_view(request):
                       publishedDate=F('Title__publishedDate'))  # 獲取出版日期信息
             .order_by('-avg_score')  # 按 review_score 遞減排序
         )
-
+        Rating_full2 = Ratings.objects.select_related('ISBN','User_ID')
+        search_results2=(
+            Rating_full2
+            .filter(Q(ISBN__Book_Title__icontains=search_key ) | Q(ISBN__ISBN__icontains=search_key))
+            .values('ISBN__ISBN')
+            .annotate(avg_score=Avg('Book_Rating'),
+                      rating_count = Count('ISBN__ISBN'),
+                      Title = F('ISBN__Book_Title'),
+                      author = F('ISBN__Book_Author'),
+                      publisher = F('ISBN__Publisher'),
+                      Year_Of_Publication = Min('ISBN__Year_Of_Publication')
+                      )
+            .order_by('-avg_score')
+        )
         # 將搜尋結果和詳細信息結合
         return render(request, 'search_result.html', {
             'data': "以下是搜尋關鍵字：" + request.POST.get('searchKey') + " 的結果",
             'context': search_results,
+            'context2': search_results2
         })
     else:
         return render(request, 'search_result.html')
